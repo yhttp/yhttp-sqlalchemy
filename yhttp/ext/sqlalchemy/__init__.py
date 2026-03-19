b5637fe28dbf4e9da877e23c7eedc14b61ce1fd7
pylover <vahid.mardani@gmail.com>
2026-03-19 13:54:46 +0400

    Fix: missing saplugins keyword argument for install function

--- yhttp/ext/sqlalchemy/__init__.py
+++ yhttp/ext/sqlalchemy/__init__.py
@@ -1,4 +1,4 @@
 from .install import install
 
 
-__version__ = '4.2.0'
+__version__ = '4.2.1'
--- yhttp/ext/sqlalchemy/install.py
+++ yhttp/ext/sqlalchemy/install.py
@@ -3,13 +3,13 @@ from yhttp.ext.dbmanager import DatabaseCommand
 from . import orm, cli
 
 
-def install(app, basemodel, db=None, cliarguments=None):
+def install(app, basemodel, db=None, cliarguments=None, saplugins=None):
     DatabaseCommand.__arguments__.append(cli.DatabaseObjectsCommand)
     if cliarguments:
         cli.DatabaseObjectsCommand.__arguments__.extend(cliarguments)
 
     if db is None:
-        db = orm.ApplicationORM(basemodel, app)
+        db = orm.ApplicationORM(basemodel, app, saplugins=None)
 
     if db.engine is None:
         @app.when
